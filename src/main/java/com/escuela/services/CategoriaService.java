920e7e1d8215be8f8e67f0ca1cf24896033fdfc4
josexrg@hotmail.com <hoj*#mj^Ist9WIoe>
2019-05-26 09:25:46 -0500

    put to update

--- src/main/java/com/escuela/services/CategoriaService.java
+++ src/main/java/com/escuela/services/CategoriaService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -52,9 +53,8 @@ public class CategoriaService {
     }
     
     @POST
-    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_HTML)
     public Response createCategoria(@PathParam("tipoCat") String tipoCat,
                                     @PathParam("descripcionCat") String descripcionCat,
                                     @PathParam("deporteIdDep") String deporteIdDep)
@@ -105,10 +105,10 @@ public class CategoriaService {
         }
     }
     
-    @POST
+    @PUT
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_HTML)
     public Response updateCategoria(@PathParam("tipoCat") String tipoCat,
                                     @PathParam("descripcionCat") String descripcionCat,
                                     @PathParam("deporteIdDep") String deporteIdDep)
